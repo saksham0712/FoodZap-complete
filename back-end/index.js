dd511317224676de3a3c8c6d17572031eff7247b
Saksham Verma <sakshamverma626@gmail.com>
2024-05-30 18:06:59 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -15,14 +15,14 @@ const cors = require('cors');
 
 const app = express();
 
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
 app.use(cors({
  origin: {"https://foodzap-server.vercel.app/"},
     method: {"POST", "GET"},
 }))
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
-
 
 app.get('/', (req, res) => {
     res.send('hello weolcome chal nikal')
